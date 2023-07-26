b723504b36b09294945170adad2692274a9bc0ec
Ziyodulla Jabborov <shlmziyo@gmail.com>
2023-07-26 13:36:07 +0200

    Forms are ready to add functionality

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,25 +1,77 @@
+import { useRef, useState } from "react";
+
 const BasicForm = (props) => {
-    return (
-      <form>
-        <div className='control-group'>
-          <div className='form-control'>
-            <label htmlFor='name'>First Name</label>
-            <input type='text' id='name' />
-          </div>
-          <div className='form-control'>
-            <label htmlFor='name'>Last Name</label>
-            <input type='text' id='name' />
-          </div>
+  const [enteredName, setEnteredName] = useState('')
+  const [enteredLastName, setEnteredLastName] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState('');
+
+  const nameChangeHandler = e => {
+    setEnteredName(e.target.value);
+  };
+
+  const lastNameChangeHandler = e => {
+    setEnteredLastName(e.target.value);
+  };
+
+  const emailChangeHandler = e => {
+    setEnteredEmail(e.target.value);
+  };
+
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    const form = {
+        enteredName,
+        enteredLastName,
+        enteredEmail
+      };
+    console.log(form);
+
+    // Clear inputs
+    setEnteredName('');
+    setEnteredLastName('');
+    setEnteredEmail('');
+  };
+
+  return (
+    <form 
+      onSubmit={submitHandler}
+    >
+      <div className='control-group'>
+        <div className='form-control'>
+          <label htmlFor='name'>First Name</label>
+          <input 
+            type='text' 
+            id='name' 
+            onChange={nameChangeHandler}
+            value={enteredName}
+          />
         </div>
         <div className='form-control'>
-          <label htmlFor='name'>E-Mail Address</label>
-          <input type='text' id='name' />
+          <label htmlFor='name'>Last Name</label>
+          <input 
+            type='text' 
+            id='name'
+            onChange={lastNameChangeHandler} 
+            value={enteredLastName}
+          />
         </div>
-        <div className='form-actions'>
-          <button>Submit</button>
-        </div>
-      </form>
-    );
-  };
-  
-  export default BasicForm;
+      </div>
+      <div className='form-control'>
+        <label htmlFor='name'>E-Mail Address</label>
+        <input 
+          type='text' 
+          id='name' 
+          onChange={emailChangeHandler}
+          value={enteredEmail}
+        />
+      </div>
+      <div className='form-actions'>
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default BasicForm;
--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,39 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const nameRef = useRef();
+
+  const nameChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+  const submitHandler = e => {
+    e.preventDefault();
+
+    if(!enteredName) return;
+    console.log(enteredName);
+
+    const enteredValue = nameRef.current.value;
+    console.log(enteredValue);
+
+    // Clear input
+    setEnteredName('');
+  };
+
     return (
-      <form>
+      <form onSubmit={submitHandler}>
         <div className='form-control'>
           <label htmlFor='name'>Your Name</label>
-          <input type='text' id='name' />
+          <input 
+            ref={nameRef}
+            type='text' 
+            id='name' 
+            onChange={nameChangeHandler}
+            value={enteredName}
+          />
         </div>
         <div className="form-actions">
-          <button>Submit</button>
+          <button type="submit">Submit</button>
         </div>
       </form>
     );
